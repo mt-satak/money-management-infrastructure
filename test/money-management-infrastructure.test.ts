f7db78b8b306203535f64dc0e1682a7a9de35a30
mt-satak <sakiware@gmail.com>
2025-08-28 09:20:59 +0900

    Initial commit

---
+++ test/money-management-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MoneyManagementInfrastructure from '../lib/money-management-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/money-management-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MoneyManagementInfrastructure.MoneyManagementInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
